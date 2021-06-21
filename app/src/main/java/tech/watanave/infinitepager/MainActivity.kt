78dbaf1db98474381a9c5fed85cc45889e565092
watanavex <y_watanabe@yumemi.co.jp>
2021-06-21 16:13:43 +0900

    Implemente infinite ViewPager

--- app/src/main/java/tech/watanave/infinitepager/MainActivity.kt
+++ app/src/main/java/tech/watanave/infinitepager/MainActivity.kt
@@ -1,14 +1,20 @@
 package tech.watanave.infinitepager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,24 +35,111 @@ class CollectionDemoFragment : Fragment(R.layout.collection_demo) {
         viewPager = view.findViewById(R.id.pager)
         viewPager.adapter = demoCollectionAdapter
 
-        val tabLayout: TabLayout = view.findViewById(R.id.tab_layout)
-        tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = "OBJECT ${(position + 1)}"
-        }.attach()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+
+        val adapter = TabAdapter()
+        recyclerView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = layoutManager
+
+        val snapHelper = SnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+
+        adapter.itemTapBlock = { position ->
+            viewPager.setCurrentItem(position, true)
+        }
+
+        val gestureDetector = GestureDetector(requireContext(), object: GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                val view = recyclerView.findChildViewUnder(e.x, e.y)!!
+                val position = recyclerView.getChildAdapterPosition(view)
+                viewPager.setCurrentItem(position, true)
+                return super.onSingleTapUp(e)
+            }
+        })
+        recyclerView.addOnItemTouchListener(object: RecyclerView.OnItemTouchListener {
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+                gestureDetector.onTouchEvent(e)
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+
+            }
+        })
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                val smoothScroller = snapHelper.getScroller(layoutManager)!!
+                smoothScroller.targetPosition = position
+
+                layoutManager.startSmoothScroll(smoothScroller)
+            }
+        })
+
+        layoutManager.scrollToPosition(Int.MAX_VALUE / 2)
+        viewPager.setCurrentItem(Int.MAX_VALUE / 2, false)
+    }
+}
+
+class SnapHelper : PagerSnapHelper() {
+
+    var onSnapViewBlock: ((Int) -> Unit)? = null
+
+    override fun findTargetSnapPosition(layoutManager: RecyclerView.LayoutManager?, velocityX: Int, velocityY: Int): Int {
+        val position = super.findTargetSnapPosition(layoutManager, velocityX, velocityY)
+        onSnapViewBlock?.invoke(position)
+        return position
+    }
+
+    fun getScroller(layoutManager: RecyclerView.LayoutManager) : RecyclerView.SmoothScroller? {
+        return this.createScroller(layoutManager)
+    }
+
+}
+
+class TabAdapter : RecyclerView.Adapter<TabAdapter.ViewHolder>() {
+
+    var itemTapBlock: ((Int) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.tab_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val value = position % 3
+        val button = holder.button
+
+        button.text = "Object $value"
+        button.setOnClickListener {
+            itemTapBlock?.invoke(position)
+        }
+    }
+
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+    class ViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val button: Button
+            get() = itemView.findViewById(R.id.button)
     }
 }
 
 class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 10
+    override fun getItemCount(): Int = Int.MAX_VALUE
 
     override fun createFragment(position: Int): Fragment {
-        // Return a NEW fragment instance in createFragment(int)
         val fragment = DemoObjectFragment()
+        val value = position % 3
         fragment.arguments = Bundle().apply {
-            // Our object is just an integer :-P
-            putInt(ARG_OBJECT, position + 1)
+            putInt(ARG_OBJECT, value)
         }
         return fragment
     }
